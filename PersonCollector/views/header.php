d28c8b7846eef0ec9fecd3200fc05d03f7245047
sontarian <duda.dariusz.dd@gmail.com>
2020-02-24 17:02:45 +0100

    Added base css formatting

--- PersonCollector/views/header.php
+++ PersonCollector/views/header.php
@@ -2,8 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HTML5 Demo</title>
     <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="css/normalize.css" />
     <!--[if IE]>
     <script src="//html5shiv.googlecode.com/svn/trunk
 /html5.js"></script>
